f51b66740234d82025c29b613ad9f5015af38190
Burak ÖZEN <burakozen91@gmail.com>
2022-03-26 00:06:15 +0300

    Initial Commit

---
+++ Simpson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simpson
+//
+//  Created by Burak ÖZEN on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
